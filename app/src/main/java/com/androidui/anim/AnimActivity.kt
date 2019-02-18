b7e97bfcc142b04b95868a8cb2290ab098cdb2b8
XiangGuYun <1069115673@qq.com>
2019-02-18 23:12:39 +0800

    大量更新

--- app/src/main/java/com/androidui/anim/AnimActivity.kt
+++ app/src/main/java/com/androidui/anim/AnimActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import com.androidui.R
 import com.kotlinlib.activity.KotlinActivity
 import com.kotlinlib.other.LayoutId
+import kotlinx.android.synthetic.main.activity_anim.*
 
 @LayoutId(R.layout.activity_anim)
 class AnimActivity : KotlinActivity() {
     override fun init(bundle: Bundle?) {
-
+        btn1.click { go(FrameAnimActivity::class.java) }
+        btn2.click { go(PatchAnimActivity::class.java) }
     }
 }
