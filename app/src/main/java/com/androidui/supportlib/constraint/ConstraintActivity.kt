4b32913b801635573f0087c0e9e1e0154b68846e
XiangGuYun <1069115673@qq.com>
2019-03-06 00:08:41 +0800

    大量更新

--- app/src/main/java/com/androidui/supportlib/constraint/ConstraintActivity.kt
+++ app/src/main/java/com/androidui/supportlib/constraint/ConstraintActivity.kt
@@ -1,13 +1,26 @@
 package com.androidui.supportlib.constraint
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.androidui.R
+import com.kotlinlib.activity.KotlinActivity
+import com.kotlinlib.other.LayoutId
+import kotlinx.android.synthetic.main.activity_constraint.*
 
-class ConstraintActivity : AppCompatActivity() {
+@LayoutId(R.layout.activity_constraint)
+class ConstraintActivity : KotlinActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_constraint)
+    override fun init(bundle: Bundle?) {
+        header3.setLeftClick { codeDialog.text("""
+
+要使某控件横向偏移百分比属性生效，必须要指定其在谁的右边和在谁的左边
+
+要使某控件竖向偏移百分比属性生效，必须要指定其在谁的下边和在谁的上边
+
+偏移的值范围是0~1
+        """.trimIndent()) }
+        btn10.click {
+            go(ConstraintCase1Activity::class.java)
+        }
     }
+
 }
